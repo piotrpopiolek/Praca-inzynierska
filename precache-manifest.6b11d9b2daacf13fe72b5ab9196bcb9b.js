31cb1d525f84e7d78b427b3ad33d1edba3b3fd23
piotrpopiolek <piotr.popiolek93@gmail.com>
2019-06-17 21:13:50 +0200

    Updates

---
+++ precache-manifest.6b11d9b2daacf13fe72b5ab9196bcb9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "436ccb997c6e3d4f1df8",
+    "url": "/Praca-inzynierska/static/js/runtime~main.436ccb99.js"
+  },
+  {
+    "revision": "02b686656ffafb55783a",
+    "url": "/Praca-inzynierska/static/js/main.02b68665.chunk.js"
+  },
+  {
+    "revision": "a1fbb8ccee426ef790ee",
+    "url": "/Praca-inzynierska/static/js/1.a1fbb8cc.chunk.js"
+  },
+  {
+    "revision": "02b686656ffafb55783a",
+    "url": "/Praca-inzynierska/static/css/main.aa6297e4.chunk.css"
+  },
+  {
+    "revision": "9faca2b7d41a593cc74d3810742757af",
+    "url": "/Praca-inzynierska/index.html"
+  }
+];
